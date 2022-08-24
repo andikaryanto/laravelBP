e7452778593643b2f5eb60d028e560382033731b
andikaryanto <41881539+andikaryanto@users.noreply.github.com>
2022-08-24 16:18:31 +0700

    product migration

---
+++ database/migrations/2022_08_24_091700_create_products.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use LaravelCommon\System\Database\Schema\Blueprint;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('description');
+            $table->unsignedBigInteger('shop_id');
+            $table->integer('rating')->unsigned()->default(0);
+            $table->boolean('is_active')->default(false);
+            $table->boolean('is_deleted')->default(false);
+            $table->dateTime('deleted_at');
+            $table->boolean('must_show')->default(true);
+            $table->auditable();
+            $table->timestamps();
+            $table->foreign('shop_id')
+                ->references('id')->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
